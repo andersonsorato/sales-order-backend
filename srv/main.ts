2ce0be5801f0dd71f14207a8cd5a538bea5fb29d
andersonsorato <andersosorato@hotmail.com>
2026-05-04 08:42:04 -0300

    feat: ajustado main ts

--- srv/main.ts
+++ srv/main.ts
@@ -17,22 +17,22 @@ export default (service: Service) => {
 service.before('CREATE', "SalesOrdersHeaders", async (request: Request) => {
     const params = request.data; 
     console.log(params);
-    if (!params.customer_id) {
-        return request.reject(404, "Missing required field: customer_id");
+    if (!params.customers_id) {
+        return request.reject(404, "Missing required field: customers_id");
     }
     if (!params.items || params.items.length === 0) {
         return request.reject(404, "Sales order must contain at least one item");
     }
-    const customerQuery = SELECT.one.from('sales.customers').where({ ID: params.customers_id });
+    const customerQuery = SELECT.one.from('sales.customers').where({ id: params.customers_id });
     const customer = await cds.run(customerQuery);
     if (!customer) {
-        return request.reject(404, "custumer not found with ID: " + params.customers_id);
+        return request.reject(404, "customer not found with ID: " + params.customers_id);
     }
     const products: string[] = params.items.map((item: SalesOrderItem) => item.products_id);
-    const productQuery = SELECT.from('sales.products').where({ ID: { in: products } });
+    const productQuery = SELECT.from('sales.products').where({ id: { in: products } });
     const productResults = await cds.run(productQuery);
-    const dbPrducts = productResults.map((product: product) => productResults.id);
-    if (!products.every((products => dbPrducts.includes(products)))) {
+    const dbPrducts = productResults.map((product: product) => product.id);
+    if (!products.every((productsId => dbPrducts.includes(productsId)))) {
         return request.reject(404, "One or more products not found with IDs: " + products.join(", "));
     }
     console.log(JSON.stringify(productQuery));
