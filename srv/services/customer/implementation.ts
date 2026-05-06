e498c8e27cfe07a72b5c44f2f89e7a5684c90452
andersonsorato <andersosorato@hotmail.com>
2026-05-06 17:58:36 -0300

    Feat: ajustado implement do service do customers

--- srv/services/customer/implementation.ts
+++ srv/services/customer/implementation.ts
@@ -5,7 +5,7 @@ import { CustomerModel } from "srv/models/customer";
 export class CustomerServiceImpl implements CustomerService {
    public afterRead(customerList: customers): customers {
         const customersS = customerList.map((customer) => {
-            const customerR = CustomerModel.create({
+            const customerR = CustomerModel.whit({
                 id: customer.id,
                 firstName: customer.firstName,
                 lastName: customer.lastName,
